3d708bc6b38e57576ef776e63bf70bef5b081040
Mahary R <maharyfenitra@gmail.com>
2023-01-02 16:21:51 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import Navbar from "./scenes/Navbar"
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home')
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
+  const isAboveMeduimScreens = useMediaQuery("(min-width: 1060px)")
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollX !== 0) selectedPage(false);
+
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  },[])
   return (
-    <div className="App">
-      
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={selectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+
+      </div>
     </div>
   );
 }
