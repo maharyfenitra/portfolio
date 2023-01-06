3c8ce44166b84e0470f0256be75ff58442187ad2
Mahary R <maharyfenitra@gmail.com>
2023-01-06 17:26:44 +0300

    Footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
 import Testimonials from "./scenes/Testimonials";
 import Contact from "./scenes/Contact"
-
+import Footer from "./scenes/Footer";
 function App() {
   const [selectedPage, setSelectedPage] = useState('home');
   const [isTopOfPage, setIsTopOfPage] = useState(true)
@@ -51,6 +51,9 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
             <Contact/>
       </div>
+      
+            <Footer/>
+      
     </div>
   );
 }
