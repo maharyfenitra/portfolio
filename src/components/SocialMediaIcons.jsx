4a3fbd9344c6a61770af68ff8801ccffab06b2d3
Mahary R <maharyfenitra@gmail.com>
2023-01-20 14:38:04 +0300

    Landing Mahary

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -4,18 +4,11 @@ const SocialMediaIcons = () => {
   return (
     <div className='flex justify-center md:justify-start my-10 gap-7'>
       <a className='hover:opacity-50 transition duration-500'
-      href='ww.linkedin.com'
+      href='https://www.linkedin.com/in/mahary-rafanomezana-86411959/'
       target='_blank'
       rel='noreferrer'
       >
         <img alt="linkedin-link" src="assets/linkedin.png"/>
-      </a>
-      <a className='hover:opacity-50 transition duration-500'
-      href='ww.twitter.com'
-      target='_blank'
-      rel='noreferrer'
-      >
-        <img alt="twitter-link" src="assets/twitter.png"/>
       </a>
       <a className='hover:opacity-50 transition duration-500'
       href='ww.facebook.com'
@@ -25,11 +18,11 @@ const SocialMediaIcons = () => {
         <img alt="facebook-link" src="assets/facebook.png"/>
       </a>
       <a className='hover:opacity-50 transition duration-500'
-      href='ww.github.com'
+      href='https://github.com/maharyfenitra'
       target='_blank'
       rel='noreferrer'
       >
-        <img alt="linkedin-link" src="assets/instagram.png"/>
+        <img alt="github" src="assets/github.png"/>
       </a>
     </div>
   )
