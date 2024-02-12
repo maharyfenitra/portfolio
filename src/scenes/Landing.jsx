19928c4104d25d3eac8749ed59e76c9efe84a67d
Mahary <maharyfenitra@gmail.com>
2024-02-12 13:47:00 +0300

    It's ok know this tme

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -12,14 +12,14 @@ const Landing = ({ setSelectedPage }) => {
             <div className='md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32'>
                 {isAboveMediumScreens ?
                     (<div className='relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 
-                before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]'>
+                before:rounded-t-[40px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]'>
                         <img alt="profile"
-                            className='hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px]'
+                            className='hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[40px]'
                             src="assets/profile-image.jpg"
                         />
                     </div>) : (<img alt="profile"
                         className='hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]
-                        rounded-t-[400px]
+                        rounded-t-[40px]
                         '
                         src="assets/profile-image.jpg"
                     />)}
